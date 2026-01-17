d28930fd14aa34ec13e1d19b13c2fca45eecf137
Manoj Kumar <manojroyal389@gmail.com>
2026-01-17 16:50:47 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE amazon_sales (
+    order_id TEXT,
+    order_date TEXT,
+    status TEXT,
+    fulfilment TEXT,
+    category TEXT,
+    sku TEXT,
+    qty INTEGER,
+    amount REAL,
+    city TEXT,
+    state TEXT,
+    country TEXT
+);
